c046ad2652cd2881b30e4afccef3deda3af2d378
Kazuho1222 <destiny5914@gmail.com>
2024-05-10 08:16:27 +0900

    JSXでマークアップを書く

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import MyButton from "./components/MyButton";
+import AboutPage from "./components/AboutPage";
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to my app</h1>
       <MyButton />
+      <AboutPage />
     </div>
   );
 }
