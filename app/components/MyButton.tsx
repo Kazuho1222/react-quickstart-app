c20192cd9ae8cae5cfa935dc1a025e7e8039a156
Kazuho1222 <destiny5914@gmail.com>
2024-05-12 00:21:20 +0900

    イベントに応答する

--- app/components/MyButton.tsx
+++ app/components/MyButton.tsx
@@ -1,6 +1,14 @@
+"use client"
+
 function MyButton() {
+  function handleClick() {
+    alert("You clicked me!");
+  }
+
   return (
-    <button>I'm a button</button>
+    <button onClick={handleClick}>
+      Click me
+    </button>
   );
 }
 
